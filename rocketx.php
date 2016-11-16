d2a08ab4b01250589c4764f309d5df1bf88db053
T568A <andrey.lopatkin@outlook.com>
2016-11-16 21:16:40 +0500

    delete class

--- rocketx.php
+++ rocketx.php
@@ -1,13 +1,13 @@
 <?php
 
 use Rocketx\App\{
-    ConcatConfig, Make, PrintCli
+    Config, Make, PrintCli
 };
 
 require_once __DIR__ . '/bootstrap/autoload.php';
 
 try {
-    $config = (new ConcatConfig())->getConfig();
+    $config = (new Config())->getConfig();
     if (!empty($config->script->help)) {
         PrintCli::getHelp();
     } else if (!empty($config->script->list)) {
--- src/App/Config.php
+++ src/App/Config.php
@@ -4,10 +4,12 @@ namespace Rocketx\App;
 
 class Config
 {
-    protected $configFile;
-    protected $optionsCli;
+    private $configFile;
+    private $optionsCli;
+    private $domain;
+    private $template;
 
-    protected function getSettingsFile()
+    private function getSettingsFile()
     {
         $this->configFile = json_decode(file_get_contents('config/config.json'));
         if (empty((array)$this->configFile)) {
@@ -15,7 +17,7 @@ class Config
         }
     }
 
-    protected function getOptionsCli()
+    private function getOptionsCli()
     {
         $options = [
             'd:' => 'domain:',
@@ -24,5 +26,44 @@ class Config
             'l::' => 'list::'
         ];
         $this->optionsCli = getopt(implode('', array_keys($options)), $options);
+
+        if (empty($this->optionsCli)) {
+            fwrite(STDOUT, 'Enter Domain (example.com):');
+            $line1 = trim(fgets(STDIN));
+            fwrite(STDOUT, 'Enter Template (example.tmp):');
+            $line2 = trim(fgets(STDIN));
+            $this->domain = (!empty($line1)) ? $line1 : 'example.com';
+            $this->template = (!empty($line2)) ? $line2 : 'example.tmp';
+        } else {
+            $this->domain = $this->optionsCli['domain'] ?? $this->optionsCli['d'] ?? NULL;
+            $this->template = $this->optionsCli['template'] ?? $this->optionsCli['t'] ?? NULL;
+        }
+
+        if (isset($this->optionsCli['help']) || isset($this->optionsCli['h'])) {
+            $this->configFile->script->help = true;
+        }
+
+        if (isset($this->optionsCli['list']) || isset($this->optionsCli['l'])) {
+            $this->configFile->script->list = true;
+        }
+
+    }
+
+    public function getConfig()
+    {
+        $this->getSettingsFile();
+        $this->getOptionsCli();
+
+        if (!empty($this->configFile->script->help) || !empty($this->configFile->script->list)) {
+            return $this->configFile;
+        }
+
+        if (!empty($this->domain) && !empty($this->template)) {
+            $this->configFile->site->domain = $this->domain;
+            $this->configFile->script->nameTemplateFile = $this->template;
+            return $this->configFile;
+        } else {
+            return false;
+        }
     }
 }
